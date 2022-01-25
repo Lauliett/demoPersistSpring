81e8a67c4b563f8a65578ee6cced4edfc141ac7d
Lauliett <lauragimenocp@gmail.com>
2022-01-25 20:43:50 +0100

    ale ale!

--- src/main/java/com/lauliett/demoREST_persist/controlador/PSP_ProductoControlador.java
+++ src/main/java/com/lauliett/demoREST_persist/controlador/PSP_ProductoControlador.java
@@ -25,10 +25,11 @@ public class PSP_ProductoControlador {
 	 * 
 	 * @return
 	 */
+    //Donde va estar nuestro servicio REST para obtener el producto
 	@GetMapping("/producto")
 	public List<PSP_Producto> obtenerTodos() {
-		// Vamos a modificar este código
-		return null;
+		
+		return productoRepositorio.findAll();
 	}
 
 	/**
@@ -37,8 +38,10 @@ public class PSP_ProductoControlador {
 	 * @param id
 	 * @return Null si no encuentra el producto
 	 */
+    /*Significa que vamos a obtener un producto concreto de la base de datos!*/
 	@GetMapping("/producto/{id}")
-	public PSP_Producto obtenerUno(@PathVariable Long id) {
+	//Por eos lo anotamos como PathVariable
+    public PSP_Producto obtenerUno(@PathVariable Long id) {
 		// Vamos a modificar este código
 		return null;
 	}
@@ -49,10 +52,11 @@ public class PSP_ProductoControlador {
 	 * @param nuevo
 	 * @return producto insertado
 	 */
+    /* Post producto*/
 	@PostMapping("/producto")
 	public PSP_Producto nuevoProducto(@RequestBody PSP_Producto nuevo) {
 		// Vamos a modificar este código
-		return null;
+		return productoRepositorio.save(nuevo);
 	}
 
 	/**
