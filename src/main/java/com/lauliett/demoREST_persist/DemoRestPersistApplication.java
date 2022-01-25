61757adc29969b4d61cae32051857bba051bb63b
Lauliett <lauragimenocp@gmail.com>
2022-01-25 19:35:20 +0100

    publicateeeeee

---
+++ src/main/java/com/lauliett/demoREST_persist/DemoRestPersistApplication.java
@@ -0,0 +1,12 @@
+package com.lauliett.demoREST_persist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRestPersistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRestPersistApplication.class, args);
+	}	
+}
---
+++ src/test/java/com/lauliett/demoREST_persist/DemoRestPersistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lauliett.demoREST_persist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRestPersistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
