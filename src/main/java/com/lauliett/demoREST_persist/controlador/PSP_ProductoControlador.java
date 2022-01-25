dc3ed5ffa7bb1303a0fd22bbf1f237a65538aa80
Lauliett <lauragimenocp@gmail.com>
2022-01-25 21:08:43 +0100

    mensaje

--- src/main/java/com/lauliett/demoREST_persist/controlador/PSP_ProductoControlador.java
+++ src/main/java/com/lauliett/demoREST_persist/controlador/PSP_ProductoControlador.java
@@ -41,9 +41,8 @@ public class PSP_ProductoControlador {
     /*Significa que vamos a obtener un producto concreto de la base de datos!*/
 	@GetMapping("/producto/{id}")
 	//Por eos lo anotamos como PathVariable
-    public PSP_Producto obtenerUno(@PathVariable Long id) {
-		// Vamos a modificar este código
-		return null;
+    public PSP_Producto obtenerUno(@PathVariable Long id) {		
+		return productoRepositorio.findById(id).orElse(null);
 	}
 
 	/**
@@ -67,18 +66,30 @@ public class PSP_ProductoControlador {
 	 */
 	@PutMapping("/producto/{id}")
 	public PSP_Producto editarProducto(@RequestBody PSP_Producto editar, @PathVariable Long id) {
-		// Vamos a modificar este código
-		return null;
+		if(productoRepositorio.existsById(id)){
+			editar.setId(id);
+			return productoRepositorio.save(editar);
+		}else{
+			return null;
+		}
 	}
 
 	/**
+	 * Es un estandar de REST que cuando tu borras algo devuelves lo borrado!
 	 * Borra un producto del catálogo en base a su id
 	 * @param id
 	 * @return
 	 */
 	@DeleteMapping("/producto/{id}")
 	public PSP_Producto borrarProducto(@PathVariable Long id) {
-		// Vamos a modificar este código
-		return null;
+		
+		PSP_Producto producto = null;
+
+		if(productoRepositorio.existsById(id)){
+			producto = productoRepositorio.findById(id).get();			
+			productoRepositorio.deleteById(id);			
+		}
+
+		return producto;
 	}	
 }
